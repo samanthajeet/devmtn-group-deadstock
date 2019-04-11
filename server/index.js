95cecbc12123398bfa2968590414ced33ccf936a
Joseph Hamblin <joseph.jay.hamblin@gmail.com>
2019-04-11 10:35:09 -0600

    aws s3, uploader, dashboard format change, index dependencies dropzone

--- server/index.js
+++ server/index.js
@@ -37,6 +37,43 @@ massive(CONNECTION_STRING).then(db => {
   });
 });
 
+//Amazon S3
+const aws = require('aws-sdk');
+
+const { S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } = process.env;
+
+app.get('/api/signs3', (req, res) => {
+ aws.config = {
+   region: 'us-west-1',
+   accessKeyId: AWS_ACCESS_KEY_ID,
+   secretAccessKey: AWS_SECRET_ACCESS_KEY,
+ };
+
+ const s3 = new aws.S3();
+ const fileName = req.query['file-name'];
+ const fileType = req.query['file-type'];
+ const s3Params = {
+   Bucket: S3_BUCKET,
+   Key: fileName,
+   Expires: 60,
+   ContentType: fileType,
+   ACL: 'public-read',
+ };
+
+ s3.getSignedUrl('putObject', s3Params, (err, data) => {
+   if (err) {
+     console.log(err);
+     return res.end();
+   }
+   const returnData = {
+     signedRequest: data,
+     url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`,
+   };
+
+   return res.send(returnData);
+ });
+});
+
 // Auth Endpoints
 const authCtrl = require("./Controllers/AuthController");
 app.post(`/api/auth/register`, authCtrl.register);
