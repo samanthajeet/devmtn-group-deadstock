8498a54bb01799af85afcf6566ac18d47ec80f52
Kevin Ung <ung.kevin78@gmail.com>
2019-04-15 15:04:09 -0600

    set up redux,testing, and logic framework

---
+++ src/Testing/logickevin.test.js
@@ -0,0 +1,24 @@
+// import {logout} from '../Logic/logickevin'
+// import axios from 'axios';
+// import MockAdapter from 'axios-mock-adapter';
+
+// describe('Tests logout function', () => {
+//     it('should be a function',()=>{
+//       expect(typeof logout).toBe('function')
+//     });
+    
+//     it('should be a boolean',()=>{
+//       const result =logout()
+//       expect(typeof result).toBe('boolean')
+//     });
+    
+//     // it('should return false if given true',()=>{
+//     //   const result = toggle(true);
+//     //   expect(result).toBe(false);
+//     // });
+  
+//     // it('should return true if given a falsey value',()=>{
+//     //   const result = toggle('');
+//     //   expect(result).toBeTruthy();
+//     // })
+//   });
